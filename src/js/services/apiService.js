decfc93af516e3b56e931eba50eb44aa6061e97f
Alex Khomenko <sasvsha@gmail.com>
2020-08-13 20:05:51 +0200

    updated movieById  and export{}

--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Ключик пока положил сюда, потом решим где будем хранить 😋
 const key = '315e08344ecf67d3b7c1eb2e8ad237c9';
-// Первый подход
+
 const getGenres = function () {
   const genres = {};
   axios
@@ -16,7 +16,6 @@ const getGenres = function () {
     );
   return genres;
 };
-
 const getMoviesByInput = function (keyword, currentPage = 1) {
   return axios
     .get(
@@ -29,25 +28,17 @@ const getMoviesByInput = function (keyword, currentPage = 1) {
       throw new Error(error);
     }); // Прокинул error. Может есть более оптимальный вариант, так как я еще не работал с axios:)
 };
-// Второй подход
-const getMoviesArray = async function (keyword, currentPage = 1) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${keyword}&page=${currentPage}`,
-  );
-  const movies = response.data.results.reduce((acc, movie) => {
-    getMovieById(movie.id, acc);
-    return acc;
-  }, []);
-  console.log(movies);
-};
-const getMovieById = async function (movieId, array) {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&language=en-US`,
-  );
-  array.push(response.data);
-  return response.data;
+const getMovieById = async function (movieId) {
+  return axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&language=en-US`,
+    )
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 };
 
-getMoviesArray('james bond');
-
-export { getMoviesByInput, getGenres };
+export { getMoviesByInput, getGenres, getMovieById };
