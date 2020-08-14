62c50e0c1e928f68bc41564e64c5d50e9a995434
Alex Khomenko <sasvsha@gmail.com>
2020-08-14 14:52:39 +0200

    export{}

--- src/js/services/localStrgService.js
+++ src/js/services/localStrgService.js
@@ -1,1 +1,33 @@
-
+class Library {
+  constructor(storage) {
+    this._storage = {
+      watched: [],
+      queue: [],
+    };
+  }
+  get watched() {
+    return this._storage.watched;
+  }
+  get queue() {
+    return this._storage.queue;
+  }
+  getFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('Library'));
+  }
+  updateLocalStorage() {
+    localStorage.setItem('Library', JSON.stringify(this._storage));
+  }
+  updateWatched(object) {
+    this._storage.watched.push(object);
+    updateLocalStorage();
+    return this._storage.object;
+  }
+  updateQueue(object) {
+    this._storage.queue.push(object);
+    updateLocalStorage();
+    return this._storage.queue;
+  }
+  updateStorage() {
+    this._storage = this.getFromLocalStorage();
+  }
+}
