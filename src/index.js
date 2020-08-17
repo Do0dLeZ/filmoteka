07eb3df0e0c9c0e9e4c610cbdf02d20a0e16936e
Eugene Kolomiiets <kolomiets1990@gmail.com>
2020-08-17 17:47:01 +0300

    Card click listener for Michail

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import './sass/main.scss';
 
 import { renderCardsAndPagination as renderHomeCards } from './js/components/renderCards.js';
 import { renderHeaderHome } from './js/components/renderHeader.js';
-import { requestPopularMovies } from './js/services/apiService.js';
+import {
+  requestPopularMovies,
+  requestMovieByID,
+} from './js/services/apiService.js';
+
+// ============================ MAIN PARAMS ======================================
+
+let homePage = 1;
+let watchedPage = 1;
+let queuePage = 1;
 
 const refs = {
   // ============== containers ==================
@@ -11,13 +20,31 @@ const refs = {
   // =============== btns/inputs ================
 };
 
+// ========================== HANDLERS  =============================
+
+const handleCardClick = e => {
+  e.preventDefault();
+  requestMovieByID(e.target.closest('.list-item').dataset.movieId)
+    .then(renderCard)
+    .finally(() => {
+      //TODO add watched/queue btn click events
+    });
+};
+
 // ========================== RENDERS   =============================
 
 const renderHomePage = () => {
   renderHeaderHome(refs.headerContainer);
-  requestPopularMovies(1).then(data =>
-    renderHomeCards(refs.mainContainer, data),
-  );
+  refs.homeBtn = document.querySelector('#link-home');
+  refs.libraryBtn = document.querySelector('#link-library');
+
+  requestPopularMovies(homePage)
+    .then(data => renderHomeCards(refs.mainContainer, data))
+    .finally(() => {
+      refs.mainContainer
+        .querySelector('.movie-list')
+        .addEventListener('click', handleCardClick);
+    });
 };
 
 // ==========================    EVENTS  =============================
